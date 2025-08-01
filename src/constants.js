a6b99564db4cd9719070b48c67cbb6e4a8111a50
abhishekjangid1 <abjangid222@gmail.com>
2025-08-01 15:46:26 +0530

    Added project files

---
+++ src/constants.js
@@ -0,0 +1,302 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+// import cssLogo from './assets/tech_logo/css.png';
+// import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+// import angularLogo from './assets/tech_logo/angular.png';
+import reduxLogo from './assets/tech_logo/redux.png';
+// import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+// import gsapLogo from './assets/tech_logo/gsap.png';
+// import materialuiLogo from './assets/tech_logo/materialui.png';
+// import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+// import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+import firebaseLogo from './assets/tech_logo/firebase.png';
+// import cLogo from './assets/tech_logo/c.png';
+// import cppLogo from './assets/tech_logo/cpp.png';
+// import javaLogo from './assets/tech_logo/java.png';
+// import pythonLogo from './assets/tech_logo/python.png';
+import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+import postmanLogo from './assets/tech_logo/postman.png';
+// import mcLogo from './assets/tech_logo/mc.png';
+// import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+import vercelLogo from './assets/tech_logo/vercel.png';
+// import postgreLogo from './assets/tech_logo/postgre.png';
+// import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+import webverseLogo from './assets/company_logo/webverse_logo.png';
+import agcLogo from './assets/company_logo/agc_logo.png';
+import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+
+// Education Section Logo's
+import glaLogo from './assets/education_logo/gla_logo.png';
+import bsaLogo from './assets/education_logo/bsa_logo.png';
+import vpsLogo from './assets/education_logo/vps_logo.png';
+
+// Project Section Logo's
+import githubdetLogo from './assets/work_logo/github_det.png';
+import csprepLogo from './assets/work_logo/cs_prep.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/npm.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      // { name: 'CSS', logo: cssLogo },
+      // { name: 'SASS', logo: sassLogo },
+      // { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      // { name: 'Angular', logo: angularLogo },
+      { name: 'Redux', logo: reduxLogo },
+      // { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      // { name: 'GSAP', logo: gsapLogo },
+      // { name: 'Material UI', logo: materialuiLogo },
+      // { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      // { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      { name: 'Firebase', logo: firebaseLogo },
+      // { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      // { name: 'C', logo: cLogo },
+      // { name: 'C++', logo: cppLogo },
+      // { name: 'Java', logo: javaLogo },
+      // { name: 'Python', logo: pythonLogo },
+      // { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      { name: 'Postman', logo: postmanLogo },
+      // { name: 'Compass', logo: mcLogo },
+      { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      // { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    // {
+    //   id: 0,
+    //   img: webverseLogo,
+    //   role: "Fullstack Developer",
+    //   company: "Webverse Digital",
+    //   date: "April 2024 - Present",
+    //   desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+    //   skills: [
+    //     "HTML",
+    //     "CSS",
+    //     "JavaScript",
+    //     "React JS",
+    //     "TypeScript",
+    //     "Node JS",
+    //     "Tailwind CSS",
+    //     "MongoDb",
+    //     "Redux",
+    //     " Next Js",
+    //   ],
+    // },
+    {
+      id: 1,
+      img: agcLogo,
+      role: "Web Developer",
+      company: "Brainwave Matrix Solutions",
+      date: "March 2025 - May 2025",
+      desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+      skills: [
+        "ReactJS",
+        "Redux",
+        "JavaScript",
+        "Tailwind CSS",
+        "Mongodb",
+        "ExpressJS",
+        "Nodejs",
+      ],
+    },
+    {
+      id: 2,
+      img: newtonschoolLogo,
+      role: "NodeJs Developer",
+      company: "Cloudcredits Technologies Pvt. Ltd.",
+      date: "January 2025 - February 2025",
+      desc: "Worked as a Backend Developer Intern, developed API's Using Nodejs with ExpressJS and intergrated  with the  help of  frontend UI Library ReactJS components and Database MongoDB for data storage .",
+      skills: [
+        "Nodejs",
+        "ExpressJS",
+        "MongoDb",
+        "Javascript",
+        "TailwindCSS",
+        "HTML",
+        "CSS",
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: glaLogo,
+      school: "Apex University, Jaipur",
+      date: "Sept 2021 - July 2024",
+      grade: "7.23 CGPA",
+      desc: "I have completed my Bachelor's degree (BCA) in Computer Applications from Apex University, Jaipur. During my time at AU, I gained a strong foundation in programming, software development, and computer science principles. I have studied courses such as Web development, Data Structures, Algorithms, Object-Oriented Programming, Database Management Systems and Softwares. I actively participated in various workshops and technical events, which enhanced my skills and knowledge. My experience at Apex University has been instrumental in shaping my technical abilities and professional growth.",
+      degree: "Bachelor of Computer Applications - BCA",
+    },
+    // {
+    //   id: 1,
+    //   img: bsaLogo,
+    //   school: "BSA College, Mathura",
+    //   date: "Sept 2018 - Aug 2021",
+    //   grade: "73.2%",
+    //   desc: "I completed my Bachelor's degree in Computer Science (B.Sc.) from BSA College, Mathura. Throughout my studies, I was immersed in a variety of subjects that deepened my understanding of computing and technology. From exploring Data Structures and Algorithms to diving into Web Development and Database Management Systems, I gained practical insights into the world of software development. My time at BSA College allowed me to work on projects that applied theoretical concepts to real-world problems.",
+    //   degree: "Bachelor of Science - BSC (Computer Science)",
+    // },
+    // {
+    //   id: 2,
+    //   img: vpsLogo,
+    //   school: "Vatsalya Public School Govardhan, Mathura",
+    //   date: "Apr 2017 - March 2018",
+    //   grade: "78%",
+    //   desc: "I completed my class 12 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+    //   degree: "CBSE(XII) - PCM with Computer Science",
+    // },
+    // {
+    //   id: 3,
+    //   img: vpsLogo,
+    //   school: "Vatsalya Public School Govardhan, Mathura",
+    //   date: "Apr 2015 - March 2016",
+    //   grade: "87.5%",
+    //   desc: "I completed my class 10 education from Vatsalya Public School, Govardhan, under the CBSE board, where I studied Science with Computer.",
+    //   degree: "CBSE(X), Science with Computer Application",
+    // },
+  ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "BloggingWebApp",
+      description:
+        "A full-stack blogging platform with user roles and authentication.which build to secure system with JWT and enable CRUD functionality .",
+      image: githubdetLogo,
+      tags: ["Nodejs", "ExpressJS", "JavaScript", "React JS", "MongoDb"],
+      github: "https://github.com/abhishekjangid17/BloggingWebApp",
+      webapp: "https://mern-bookstore-livid.vercel.app/",
+    },
+    {
+      id: 1,
+      title: "MERN Bookstore",
+      description:
+        "Online Bookstore website, user can easily add books and browser books after Signup,login as well as Implemented backend logic to support real-time UI responsiveness via Zustand-managed state on frontend",
+      image: csprepLogo,
+      tags: ["React JS", "Node.js", "MongoDB", "Express", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/abhishekjangid17/mern-bookstore",
+      webapp: "https://mern-bookstore-livid.vercel.app/",
+    },
+    {
+      id: 2,
+      title: "AJ_RentalCars",
+      description:
+        "A React-based web application that provides Cars browsing and book for rent and can contact us. It only used frontend part and email contact functionality",
+      image: movierecLogo,
+      tags: ["React JS", "API", "HTML", "CSS", "JavaScript"],
+      github: "https://github.com/abhishekjangid17/AJ_RentalCars",
+      webapp: "https://ajcars1.netlify.app/",
+    },
+    {
+      id: 3,
+      title: "Awesome Todo App",
+      description:
+        "A responsive and minimalistic task management app that focuses on create,read,delete. used CRUD functionality with Dynamic UI with Tailwind CSS.",
+      image: npmLogo,
+      tags: ["React JS", "Node.js", "NPM", "Validation", "MongoDb"],
+      github: "https://github.com/abhishekjangid17/TodoApp",
+      webapp: "https://todoapp-dx6g.onrender.com/",
+    },
+    {
+      id: 4,
+      title: "Task Reminder Chrome Extension Tool",
+      description:
+        "A productivity-boosting Chrome extension designed to help users manage and remember their daily tasks. Built using JavaScript, it offers a simple interface with reminders and task notifications to keep users on track.",
+      image: taskremLogo,
+      tags: ["JavaScript", "Chrome Extension", "HTML", "CSS"],
+      github: "https://github.com/codingmastr/Task-Reminder-Tool",
+      webapp: "chrome://extensions/?id=kngheeibjnnidhfoomkpnbeghackheci",
+    },
+    {
+      id: 5,
+      title: "Calculator-WebProject",
+      description:
+        "This is Web Project which is based on mern stack whic provide functionality to find output of any calculation and it store the calculation as a history which show in UI part.",
+      image: webverLogo,
+      tags: ["HTML", "CSS", "JavaScript", "MongoDB"],
+      github: "https://github.com/abhishekjangid17/CloudCredits_Calculator",
+      webapp: "https://cloudcredits-calculator.onrender.com/",
+    },
+    {
+      id: 6,
+      title: "Day Panner Project",
+      description:
+        "A Blog Mern proejct which is work as by creating blog and can access profile with their Blogs and this projects use firebase as well for the authentication.",
+      image: cmLogo,
+      tags: ["React JS", "Node.js", "MongoDB", "Express", "Firebase"],
+      github: "https://github.com/abhishekjangid17/BlogWebsite",
+      webapp: "https://blogify-1-2yhi.onrender.com/",
+    },
+    // {
+    //   id: 7,
+    //   title: "Image Search App",
+    //   description:
+    //     "A React.js-based image search application that allows users to search and download high-quality images from the web. Built using external APIs to ensure a vast library of results for various queries.",
+    //   image: imagesearchLogo,
+    //   tags: ["React JS", "API", "Search Feature", "CSS", "Javascript"],
+    //   github: "https://github.com/codingmastr/Image-Search-App",
+    //   webapp: "https://imagsearch.netlify.app/",
+    // },
+    // {
+    //   id: 8,
+    //   title: "Image Background Remover",
+    //   description:
+    //     "An efficient background removal app built with React.js and API integration. Users can upload any image, remove the background, and download the transparent version for further use.",
+    //   image: removebgLogo,
+    //   tags: ["React JS", "API", "Image Processing", "HTML", "CSS", "Javascript"],
+    //   github: "https://github.com/codingmastr/Image-Background-Remover",
+    //   webapp: "https://removeyourbg.netlify.app/",
+    // },
+  ];  
